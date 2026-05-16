3c29eb357f4afee49e61520a7ad321a20f0bf9a1
Mike Wallio <michael.wallio@outlook.com>
2026-05-15 21:28:23 -0400

    Add filter for engine type

--- extensions/omlx.ts
+++ extensions/omlx.ts
@@ -3,6 +3,10 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 const OMLX_BASE_URL = process.env.OMLX_BASE_URL ?? "http://localhost:8000";
 const OMLX_API_KEY = process.env.OMLX_API_KEY ?? "omlx";
 
+function isLLM(engineType: string): engineType is "llm" | "vlm" {
+  return engineType === "llm" || engineType === "vlm";
+}
+
 async function fetchOMLXModels() {
   try {
     const response = await fetch(`${OMLX_BASE_URL}/v1/models/status`, {
@@ -36,16 +40,18 @@ export default async function (pi: ExtensionAPI) {
     baseUrl: `${OMLX_BASE_URL}/v1`,
     apiKey: OMLX_API_KEY,
     api: "openai-completions",
-    models: payload.models.map((model) => {
-      return {
-        id: model.id,
-        name: model.id,
-        reasoning: true,
-        input: model.engine_type === "vlm" ? ["text", "image"] : ["text"],
-        cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
-        contextWindow: model.max_context_window,
-        maxTokens: model.max_tokens,
-      };
-    }),
+    models: payload.models
+      .filter((m) => isLLM(m.engine_type))
+      .map((model) => {
+        return {
+          id: model.id,
+          name: model.id,
+          reasoning: true,
+          input: model.engine_type === "vlm" ? ["text", "image"] : ["text"],
+          cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
+          contextWindow: model.max_context_window,
+          maxTokens: model.max_tokens,
+        };
+      }),
   });
 }
